09953f3403a5eca132cce05e76fb1b1b33dbcd12
sale-events-dev <contact@sale-events.com>
2026-07-10 13:48:50 +0700

    Create main.js

---
+++ main.js
@@ -0,0 +1,27 @@
+const { app, BrowserWindow } = require('electron');
+
+let mainWindow;
+
+app.on('ready', () => {
+  mainWindow = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  });
+
+  mainWindow.loadURL('https://sale-events.com/');
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
